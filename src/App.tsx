09ea9448c9c72c37ea6e1788c1bf67632bf89f1d
VovaSv <vovasvidi@gmail.com>
2024-10-27 12:40:44 +0200

    split screen pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import './App.css';
 import { SplitScreen } from './components/split-screen';
 
+const LeftSideComp = ({ title }: { title?: string }) => {
+	return <h2 style={{ backgroundColor: 'green' }}>{`I am Left ${title}`}</h2>;
+};
+
+const RightSideComp = ({ title = '' }: { title?: string }) => {
+	return <h2 style={{ backgroundColor: 'red' }}>{`I am Right ${title}`}</h2>;
+};
+
 function App() {
-  return <SplitScreen />;
+	return (
+		// <SplitScreen
+		// 	Left={LeftSideComp}
+		// 	Right={RightSideComp}
+		// 	leftWidth={1}
+		// 	rightWidth={3}
+		// />
+
+		<SplitScreen leftWidth={1} rightWidth={3}>
+			<LeftSideComp />
+			<RightSideComp />
+		</SplitScreen>
+	);
 }
 
 export default App;
--- src/components/split-screen.tsx
+++ src/components/split-screen.tsx
@@ -1,3 +1,43 @@
-export const SplitScreen = ({}) => {
-  return 'Split Screen';
+import React from 'react';
+import { styled } from 'styled-components';
+
+interface PanelProps {
+	flex?: number;
+}
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Panel = styled.div<PanelProps>`
+	flex: ${(props) => props?.flex};
+`;
+
+export const SplitScreen = ({
+	Left,
+	Right,
+	leftWidth = 1,
+	rightWidth = 1,
+	children,
+}: //children,
+{
+	Left: any;
+	Right: any;
+	leftWidth: number;
+	rightWidth: number;
+	children: React.ReactNode[];
+}) => {
+	const [left, right] = children;
+	return (
+		<Container>
+			{/* <Panel flex={leftWidth}>
+				<Left title={'asdd'} />
+			</Panel>
+			<Panel flex={rightWidth}>
+				<Right />
+			</Panel> */}
+			<Panel flex={leftWidth}>{left}</Panel>
+			<Panel flex={rightWidth}>{right}</Panel>
+		</Container>
+	);
 };
